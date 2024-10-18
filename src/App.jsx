dd0a2bfd54cbfe99d0d6f4e9be7c15e0ecb6c6b5
Noemia <noemiabenkone0@gmail.com>
2024-10-18 20:50:39 -0300

    tentando fazer um post de facebook

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='content'>
+     <div className='perfil'>
+      <img src='https://www.cjgdenhaag.nl/wp-content/uploads/2022/09/Baby_donkere-huidskleur-scaled.jpg' className='foto-perfil'/>
+     </div>
+
+     <div className='post'>
+     <h2 className='nome-perfil'>my shaddai</h2>
+      <p className='legenda-post'>Amor de criança é o melhor: todos são inocentes,<br/> despreocupados e felizes.</p>
+      <img src='https://www.cdc.gov/parents/media/images/group_toddlers_1200x675.jpg' className='foto-post'/>
+     </div>
+     </div>
+    </div>
+  )
+}
+
+export default App
